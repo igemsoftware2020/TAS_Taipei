f887d41d689684e6f80215609f8c6f73f14ef8e1
usymmij <usymmij@gmail.com>
2020-04-14 13:46:27 +0800

    feat: videoFileReading

--- videoReader.py
+++ videoReader.py
@@ -3,4 +3,17 @@ import cv2
 
 vid = None
 
+def processFrame(frame, n):
+    return
+
 def openFile():
+    global vid
+    vid = cv2.VideoCapture("data/read.MOV")
+
+def readFile():
+    success, frame = vid.read()
+    frameIter = 0
+    while success:  
+        processFrame(frame, frameIter)
+        success, frame = vid.read()
+
