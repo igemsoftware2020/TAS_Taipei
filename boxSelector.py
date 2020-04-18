6992e7ccc3a04a56af1363751f894294463ee1a2
usymmij <usymmij@gmail.com>
2020-04-18 11:29:21 +0800

    feat: stores selected pixels in list data

--- boxSelector.py
+++ boxSelector.py
@@ -15,6 +15,8 @@ def mouseEvent(event,x,y,flags,param):
     global tube
     global point
 
+    # if mouse L button pressed, add the point to the points list
+    # if the vertices for a complete box has already been reached, create a new box
     if event == cv2.EVENT_LBUTTONDOWN:
         if point == 0:
             boxes.append([(x, y)])
@@ -39,39 +41,60 @@ def getMask(tb):
     p = Path(tb)
     mask = p.contains_points(points).reshape(frame.shape[0], frame.shape[1])
     mask = np.multiply(mask, 255).astype(np.uint8)
-    while True:
-        cv2.imshow("mask", mask)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+
     return mask
 
 def selectTubes(file):
     global frame
+
+    # open the first frame from the video file
     vid = cv2.VideoCapture(file)
     scc, frame = vid.read()
     vid.release()
 
+    # add mouselistener
+    cv2.namedWindow('first frame')
+    cv2.setMouseCallback('first frame',mouseEvent)
+
+    # until the use presses q, loop through this
     while True:
+
+        # render the lines
         for tb in boxes:
             if len(tb) == POINTS:
                 ppt = tb[-1]
             else:
                 ppt = tb[0]
             for pt in tb:
+
+                # add the lines to the frame
                 cv2.line(frame, ppt, pt, (255, 234, 0), thickness = 2)
                 ppt = pt
-        cv2.namedWindow('first frame')
-        cv2.setMouseCallback('first frame',mouseEvent)
+
+        # display the image or its updated form
         cv2.imshow("first frame", frame)
 
+        # if the user presses the 'q' key
         if cv2.waitKey(1) & 0xFF == ord('q'):
             masks = []
+
+            # for each box drawn, make a mask and append it to the list. Ignore if incomplete
             for tb in boxes:
                 mask = getMask(tb)
-                if mask is bool:
-                    break
-                masks.append(masks)
+                if isinstance(mask, bool) == True:
+                    return masks
+                masks.append(mask)
+            
+            # return the mask list
+            cv2.destroyAllWindows()
             return masks
 
 if __name__ == "__main__":
-    selectTubes("data/single.mov")
+    mas = selectTubes("data/single.mov")
+    
+    for m in mas:
+        while True:
+            cv2.imshow("mask", m)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    
--- videoReader.py
+++ videoReader.py
@@ -2,15 +2,37 @@ import numpy as np
 import cv2
 
 vid = None
+masks = None
+data = []
 
 def processFrame(frame, n):
-    return
-    
-def openFile(file):
-    global vid
-    vid = cv2.VideoCapture(file)
+    tube = 0
+    # for every tube (mask) that exists...
+    for mask in masks:
+        # for each mask array, add a new frame slot
+        data[tube].append([])
+        y = 0
+        for column in mask:
+            x = 0
+            for pixel in column:
+                if pixel == 255:
+                    print("pix")
+                    print(y," ", x)
+                    print(frame[y][x])
+                    data[tube][n].append(frame[y][x])
+                x += 1
+            y += 1
+        tube += 1
 
-def readFile():
+def setup(file, msks):
+    global vid
+    global masks
+    vid = cv2.VideoCapture(file)
+    masks = msks
+    for mask in masks:
+        data.append([])
+
+def parse():
     success, frame = vid.read()
     frameIter = 0
     while success:  
@@ -19,5 +41,10 @@ def readFile():
         frameIter += 1
 
 if __name__ == "__main__":
-    openFile("data/single.mov")
-    readFile()
+    vidfile = "data/single.mov"
+
+    import boxSelector
+    msks = boxSelector.selectTubes(vidfile)
+
+    setup(vidfile, msks)
+    parse()
