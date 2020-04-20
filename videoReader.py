8340415445480030613e9a3c04dc7e4c78303c60
usymmij <usymmij@gmail.com>
2020-04-20 17:12:59 +0800

    update: near completion, requires data shape and type fix

--- videoReader.py
+++ videoReader.py
@@ -3,23 +3,35 @@ import cv2
 
 vid = None
 masks = None
-data = np.asarray([])
+data = []
 
 def processFrame(frame, n):
     global data
+    dataList = []
     tube = 0
-    print(n)
     # for every tube (mask) that exists...
     for mask in masks:
         fr = frame.copy()
-#data = np.append(data, np.asarray(frame[column][pixel]))
+        fr = cv2.bitwise_and(fr, mask)
+        fr = np.ma.masked_equal(fr, [0, 0, 0])
+        fr = fr.compressed()
+        fr = np.reshape(fr, (-1, 3))
+        #print(data.shape)
+        dataList.append(fr)
         tube += 1
+    dataList = np.reshape(dataList, (-1, 1))
+    data = np.append(data, np.asarray(dataList), axis=1)
+    print(data.shape)
 
 def setup(file, msks):
     global vid
     global masks
+    global data
     vid = cv2.VideoCapture(file)
     masks = msks
+    for m in masks:
+        data.append([None])
+    data = np.asarray(data)
 
 def parse():
     success, frame = vid.read()
