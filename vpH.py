e9a7eaf4c296fffb7ae5a48ee2dbc4deda2837c2
mfang21 <58220444+mfang21@users.noreply.github.com>
2020-04-21 15:04:33 +0800

    Update vpH.py

--- vpH.py
+++ vpH.py
@@ -1,10 +1,7 @@
 import numpy
 import cv2
 
-# run 
-#   alias vp=python vpH.py -i
-# then
-#   vp
 
 if __name__ == "__main__":
-    print("g'day mate")
+    import videoReader
+    vidfile = "data/single.mov"
