ef0533bd1d4a9b513187f189c8324d6457bcde60
mfang21 <58220444+mfang21@users.noreply.github.com>
2020-04-22 14:57:38 +0800

    Update vpH.py

--- vpH.py
+++ vpH.py
@@ -1,9 +1,10 @@
 import numpy
 import cv2
 import boxSelector
-from videoReader import parse
+from videoReader import parse, setup
 
 if __name__ == "__main__":
     vidfile = "4_20 C-19 Test 3_.MOV"
-    parse()
     msks = boxSelector.selectTubes(vidfile)
+    setup(vidfile, msks)
+    parse()
